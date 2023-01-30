92e8a74dc29084eeecb912343267778a175169d6
markusVJH <markus.heinila@pp.inet.fi>
2023-01-30 19:55:51 +0200

    calculators finished

--- week_3/calculators/main.js
+++ week_3/calculators/main.js
@@ -14,17 +14,23 @@ const calcGasoline = () => {
     }
 
 
-    answer.textContent = text;
-};
-
-const tempConv = (id) => {
-    const celInput = document.getElementById('celsius');
-    const fahInput = document.getElementById('fahrenheit');
-    const kelInput = document.getElementById('kelvin');
-
-    console.log(id);
-
-    fahInput.value = celInput.value * 1.8 + 32
-    celInput.value = (fahInput.value -32) * 0.5556;
-};
-//change event on input
+    answer.textContent = text; };
+    
+    
+    const tempConv = (id) => {
+        const celInput = document.getElementById('celsius');
+        const fahInput = document.getElementById('fahrenheit');
+        const kelInput = document.getElementById('kelvin');
+    
+        if (id === "celsius") {
+            fahInput.value = (celInput.value * 9/5) + 32;
+            kelInput.value = celInput.value + 273.15;
+        } else if (id === "fahrenheit") {
+            celInput.value = (fahInput.value - 32) * 5/9;
+            kelInput.value = (fahInput.value - 32) * 5/9 +273.15;
+        } else if (id === "kelvin") {
+            celInput.value = kelInput.value - 273.15;
+            fahInput.value = (kelInput.value -273.15) * 9/5 + 32;
+        }
+        
+    };
