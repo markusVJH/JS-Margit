6871642db0e156617fafbb12dc9aa56e5593edbc
markusVJH <markus.heinila@pp.inet.fi>
2023-01-31 20:31:25 +0200

    small progress

--- week_3/pizza_task/main.js
+++ week_3/pizza_task/main.js
@@ -1,11 +1,55 @@
+
 form = document.querySelector('form');
+let total = 0;
+let size = '';
+const totalPrice = document.querySelector('#totalPrice');
+const selectedSize = document.querySelector('#selectedSize');
 
-function summary() {
+function summary(id) {
+    
+/* switch (id) {
+    case "size2":
+    total =  total + 7.5;
+    break;
+    case "size4":
+    total = total + 10.5;
+    break;
+    case "size6":
+    total = total + 12.5;
+    break;
+    case "size8":
+    total = total + 15.5
+    break;
+    default: 
+    console.log('default');
+} */
 
-console.log("hello");
-
+   if (id === "size2") {
+    total = total+7.5;
+    size = size + "2";
+} else if (id === "size4") {
+    total = total+10.5;
+    size = size + "4";
+} else if (id === "size6"){
+    total = total+12.5;
+    size = size + '6';
+} else if (id === "size8") {
+    total = total+15.5;
+    size = size + '8';
 }
 
-form.addEventListener('change', summary);
+const toppingCB = document.querySelectorAll('input[name=topping]:checked');
+const toppingsAmount = toppingCB.length;
+if (toppingsAmount > 4) {
+    total = total + 0.5;
+}
+
+totalPrice.textContent = (`Total price is ${total}`);
+selectedSize.textContent = (`Pizza size ${size}`);
+}
+
+form.addEventListener('change', function(event) {
+    summary(event.target.id);
+  });
 
 //event listener on change whole form
