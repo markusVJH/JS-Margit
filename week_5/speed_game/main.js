a47d4361c9f298a92c7a82ee12c5a88a5f76d579
markusVJH <markus.heinila@pp.inet.fi>
2023-02-21 14:25:46 +0200

    cleaned_up_begginning

--- week_5/speed_game/main.js
+++ week_5/speed_game/main.js
@@ -1,47 +1,39 @@
 const startButton = document.querySelector('#startButton')
 const circleButtons = document.querySelectorAll('.circle')
-
 const scoreDisplay = document.querySelector('#scoreDisplay')
 let scoreCount = 0
-const startGame = () => {
-  console.log('Start button is clicked')
-  event.preventDefault()
-}
+let randomNumber = 0
 
-const circleClick = () => {
-  const id = event.target.id
-  console.log(`${id} was clicked`)
-  event.preventDefault()
+const clickCircle = (i) => {
+  console.log('circle index:', i)
 
   scoreCount += 1
   console.log(scoreCount)
   scoreDisplay.textContent = scoreCount
+  event.preventDefault()
 }
-
-let previousRandomNumber = 0
-
-const randomCircle = () => {
+/* function getRandomNumber() {
+  randomNumber = Math.floor(Math.random() * 4) + 1;
+} */
+const startGame = () => {
   console.log('Start button is clicked')
   event.preventDefault()
-  for (let i = 0; i < 5; i++) {
-    let highlight = Math.floor(Math.random() * 4) + 1
-    console.log(highlight)
-  }
+  randomNumber = Math.floor(Math.random() * 4) + 1
+  console.log(randomNumber)
 }
 
-startButton.addEventListener('click', randomCircle)
-for (const circleButton of circleButtons) {
+startButton.addEventListener('click', startGame)
+
+circleButtons.forEach((circle, i) => { /* teachers way to find out which circle was clicked */
+  circle.addEventListener('click', () =>
+    clickCircle(i))
+})
+
+
+/* for (const circleButton of circleButtons) {
   circleButton.addEventListener('click', circleClick)
-}
-
-/* circles.forEach((circle, i) => { teachers way to find out which circle was clicked
-    circle.addEventListener('click', () =>
-    clickCircle(i));
-}); */
-
-/* const clickCircle = (i) => {
-  console.log('circle index:', i)
 } */
+
 
 /* notes functions
 
