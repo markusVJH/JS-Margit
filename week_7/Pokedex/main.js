cff38eb703c36b6e3385e1c9189680a99624ed8e
Markus Heinilä <markus.heinila@pp.inet.fi>
2023-03-14 15:16:37 +0200

    search_function

--- week_7/Pokedex/main.js
+++ week_7/Pokedex/main.js
@@ -1,112 +1,67 @@
 let limit = 0;
 let offset = 0;
 
+const pokeCards = (data) => {
+  // Display filtered data
+  document.querySelector('#pokeCards').innerHTML = data.map((item, i) => `<div class="pokeCard"><div class="pokeInfo">#${item.id} ${item.name}<div class="pokeType">${item.types.map(t => `<img id="type" src="https://raw.githubusercontent.com/duiker101/pokemon-type-svg-icons/5781623f147f1bf850f426cfe1874ba56a9b75ee/icons/${t.type.name}.svg" alt="${t.type.name}"/>`).join('')}</div></div><img src="${item.sprites.front_default}"/></div>`).join('');
+};
+
 function fetchData(limit, offset) {
   const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
 
   fetch(url)
-        .then(response => response.json())
-        .then(json => {
-  
-          const fetches = json.results.map(item => {
-            return fetch(item.url).then(res => res.json())
-          })
-  
-  
-          Promise.all(fetches).then(res => { dataList(res);console.log(res);})
-        })
-  
-  const dataList = (data) => {
-    {
-      document.querySelector('#pokeCards').innerHTML = data.map((item, i) => `<div class="pokeCard"><div class="pokeInfo">#${item.id} ${item.name}<div class="pokeType">${item.types.map(t => `<img id="type" src="https://raw.githubusercontent.com/duiker101/pokemon-type-svg-icons/5781623f147f1bf850f426cfe1874ba56a9b75ee/icons/${t.type.name}.svg" alt="${t.type.name}"/>`).join('')}</div></div><img src="${item.sprites.front_default}"/></div>`).join('');
-    };
-  }
-}
-const gen1 = document.querySelector('#gen1')
-const gen2 = document.querySelector('#gen2')
-const gen3 = document.querySelector('#gen3')
-const gen4 = document.querySelector('#gen4')
-const gen5 = document.querySelector('#gen5')
-const gen6 = document.querySelector('#gen6')
-const gen7 = document.querySelector('#gen7')
-const gen8 = document.querySelector('#gen8')
-const gen9 = document.querySelector('#gen9')
+  .then(response => response.json())
+  .then(json => {
+
+    const fetches = json.results.map(item => {
+      return fetch(item.url).then(res => res.json())
+    })
+
+    Promise.all(fetches).then(res => { 
+        // Clear previous data and HTML content
+      pokemonData.length = 0;
+      document.querySelector('#pokeCards').innerHTML = '';
+      // Save all pokemon data to pokemonData variable
+      pokemonData.push(...res);
+       // Display all pokemon by default
+      pokeCards(pokemonData);
+    })
+  })
+};
+
+const genButtons = document.querySelectorAll('.gen_button');
 let genTitle = document.querySelector('#genTitle')
 let introTitle = document.querySelector('#introTitle')
-let search = document.querySelector('#search')
 let dropdown = document.querySelector('.dropdown')
+const searchInput = document.querySelector('#search')
 
+const generations = [
+  { id: 1, name: 'Generation 1', limit: 151, offset: 0 },
+  { id: 2, name: 'Generation 2', limit: 100, offset: 151 },
+  { id: 3, name: 'Generation 3', limit: 135, offset: 251 },
+  { id: 4, name: 'Generation 4', limit: 107, offset: 386 },
+  { id: 5, name: 'Generation 5', limit: 156, offset: 493 },
+  { id: 6, name: 'Generation 6', limit: 72, offset: 649 },
+  { id: 7, name: 'Generation 7', limit: 88, offset: 721 },
+  { id: 8, name: 'Generation 8', limit: 96, offset: 809 },
+  { id: 9, name: 'Generation 9', limit: 110, offset: 905 },
+];
 
-gen1.addEventListener('click', function() {
-  genTitle.textContent = 'Generation 1'
-  fetchData(151,0)
-  introTitle.style.display = 'none'
-  search.style.display = 'block'
-  dropdown.style.display = 'inline-block'
+genButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const genId = parseInt(button.dataset.genId);
+    const generation = generations.find(gen => gen.id === genId);
+    genTitle.textContent = generation.name;
+    fetchData(generation.limit, generation.offset);
+    introTitle.style.display = 'none';
+    search.style.display = 'flex';
+    dropdown.style.display = 'inline-block';
+  });
 });
-gen2.addEventListener('click', function() {
-  genTitle.textContent = 'Generation 2'
-  fetchData(100,151)
-  introTitle.style.display = 'none'
-  search.style.display = 'flex'
-  dropdown.style.display = 'inline-block'
-});
-gen3.addEventListener('click', function() {
-  genTitle.textContent = 'Generation 3'
-  fetchData(135,251)
-  introTitle.style.display = 'none'
-  search.style.display = 'flex'
-  dropdown.style.display = 'inline-block'
-});
-gen4.addEventListener('click', function() {
-  genTitle.textContent = 'Generation 4'
-  fetchData(107,386)
-  introTitle.style.display = 'none'
-  dropdown.style.display = 'inline-block'
-});
-gen5.addEventListener('click', function() {
-  genTitle.textContent = 'Generation 5'
-  fetchData(156,493)
-  introTitle.style.display = 'none'
-  search.style.display = 'flex'
-  dropdown.style.display = 'inline-block'
-});
-gen6.addEventListener('click', function() {
-  genTitle.textContent = 'Generation 6'
-  fetchData(72,649)
-  introTitle.style.display = 'none'
-  search.style.display = 'flex'
-  dropdown.style.display = 'inline-block'
-});
-gen7.addEventListener('click', function() {
-  genTitle.textContent = 'Generation 7'
-  fetchData(88,721)
-  introTitle.style.display = 'none'
-  search.style.display = 'flex'
-  dropdown.style.display = 'inline-block'
-});
-gen8.addEventListener('click', function() {
-  genTitle.textContent = 'Generation 8'
-  fetchData(96,809)
-  introTitle.style.display = 'none'
-  search.style.display = 'flex'
-  dropdown.style.display = 'inline-block'
-});
-gen9.addEventListener('click', function() {
-  genTitle.textContent = 'Generation 9'
-  fetchData(110,905)
-  introTitle.style.display = 'none'
-  search.style.display = 'flex'
-  dropdown.style.display = 'inline-block'
-});
-
-
-
 //searchpokemon
 let searchValue = ''
 const pokemonData = [];
 let filterList;
-
 const searchPokemon = (e) => {
   searchValue = e.target.value;
   filterList =
@@ -117,3 +72,23 @@ const searchPokemon = (e) => {
   : pokemonData;
   pokeCards(filterList);
 };
+
+const dropdownBtn = document.getElementById('dropdownBtn');
+dropdownBtn.addEventListener('click', (e) => {
+  if (e.target.tagName === 'A') {
+    const type = e.target.dataset.type;
+    filterList = pokemonData.filter((pokemon) => {
+      return pokemon.types.some((t) => t.type.name === type);
+    });
+    pokeCards(filterList);
+  }
+});
+
+const typeLinks = document.querySelectorAll('.dropdown-content a');
+searchInput.addEventListener('input', searchPokemon);
+typeLinks.forEach(link => {
+  link.addEventListener('click', (e) => {
+    const type = link.textContent.toLowerCase();
+    searchPokemon(e, type);
+  });
+});
